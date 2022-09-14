c4da8b1b7f7f0cda18d83615784327d3c378f48e
andresmarpz <andresmarpz@gmail.com>
2022-09-14 17:15:10 -0300

    feat: add more tests

--- packages/favicons/tests/url.test.ts
+++ packages/favicons/tests/url.test.ts
@@ -1,23 +1,43 @@
-import { processURL } from '../src';
+import { processURL } from "../src";
 
 describe("test processURL(url: string)", () => {
 	it("should maintain correct url", () => {
-		expect(processURL('https://google.com')).toBe("https://google.com");
+		expect(processURL("https://google.com")).toBe("https://google.com");
 	});
 
 	it("should add protocol", () => {
-		expect(processURL('google.com')).toBe("https://google.com");
-	})
-
-	it("should remove trailing slash", () => {
-		expect(processURL('https://google.com/')).toBe("https://google.com");
-	})
+		expect(processURL("google.com")).toBe("https://google.com");
+	});
 
 	it("should remove trailing slash and add protocol", () => {
-		expect(processURL('google.com/')).toBe("https://google.com");
+		expect(processURL("google.com/")).toBe("https://google.com");
+	});
+
+	it("should add protocol and remove parameters", () => {
+		expect(processURL("google.com/?test=1")).toBe("https://google.com");
+	});
+
+	it("should add protocol to url with subdomain", () => {
+		expect(processURL("www.google.com")).toBe("https://www.google.com");
+	});
+
+	it("should add protocol to url with subdomain 2", () => {
+		expect(processURL("test.google.com")).toBe("https://test.google.com");
+	});
+
+	it("should remove trailing slash and add protocol to url with subdomain", () => {
+		expect(processURL("www.google.com/")).toBe("https://www.google.com");
+	});
+
+	it("should remove trailing slash", () => {
+		expect(processURL("https://google.com/")).toBe("https://google.com");
+	});
+
+	it("should remove extra trailing slashes", () => {
+		expect(processURL("https://google.com///")).toBe("https://google.com");
 	});
 
 	it("should remove extra parameters", () => {
-		expect(processURL('https://google.com/?test=1')).toBe("https://google.com");
-	})
+		expect(processURL("https://google.com/?test=1")).toBe("https://google.com");
+	});
 });
