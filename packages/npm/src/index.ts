fdd11af463cf12cf3be9f7f75dc8c0db4e7fe973
andresmarpz <andresmarpz@gmail.com>
2022-09-15 21:25:22 -0300

    fix: add missing disable-sandbox flags on puppeteer

--- packages/npm/src/index.ts
+++ packages/npm/src/index.ts
@@ -45,7 +45,9 @@ async function withRequest(url: string) {
 }
 
 async function withBrowser(url: string): Promise<Favicon[]> {
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({
+		args: ['--no-sandbox', '--disable-setuid-sandbox'],
+	});
 	const page = await browser.newPage();
 	await page.goto(url, { timeout: 10000 });
 
