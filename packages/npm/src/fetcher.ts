86c295f4a4488a0c12a11d4f2eb1f43dad33bf85
Andrés Martínez <andresmarpz@gmail.com>
2023-02-06 21:08:57 -0300

    add typescript docs, export modular functions to reuse requests

--- packages/npm/src/fetcher.ts
+++ packages/npm/src/fetcher.ts
@@ -2,9 +2,30 @@ import { fetchWithTimeout } from "./util";
 import type { Favicon } from "./types";
 import { isRelativeURL, processURL } from "./util";
 
-const paths = ["/favicon.ico", "/favicon.png", "/favicon.svg", "/apple-touch-icon.png"];
+const paths = [
+	"/favicon.ico",
+	"/favicon.png",
+	"/favicon.gif",
+	"/favicon.jpg",
+	"/favicon.jpeg",
+	"/favicon.svg",
+	"/apple-touch-icon.png",
+];
 
-async function getFaviconFrom(url: string) {
+/**
+ * Get the Favicon object from the given URL
+ *
+ * @example
+ * ```ts
+ * import { getFaviconFrom } from "favicons";
+ *
+ * const favicon = await getFaviconFrom("https://nextjs.com/favicon.ico");
+ * ```
+ *
+ * @param url @description The URL to get the favicon object from
+ * @returns Promise<Favicon>
+ */
+export async function getFaviconFrom(url: string) {
 	const response = await fetchWithTimeout(url);
 	if (!response || !response.ok) return undefined;
 
@@ -20,10 +41,26 @@ async function getFaviconFrom(url: string) {
 	return { url, size, extension } as Favicon;
 }
 
-export async function requestFavicons(url: string, hrefs: string[]) {
+/**
+ * Get favicon objects from the given hrefs
+ *
+ * @example
+ * ```ts
+ * import { requestFavicons } from "favicons";
+ *
+ * const favicons = await requestFavicons(["https://nextjs.com/favicon.ico", "https://nextjs.com/favicon.png"]);
+ * ```
+ *
+ * @param hrefs @description The list of hrefs to get the favicon objects from
+ * @param url @description The URL to use as a base for relative paths
+ * @returns Promise<Favicon[]>
+ */
+export async function getFaviconsFrom(hrefs: string[], url?: string) {
 	const filteredPaths = paths.map((path) => url + path).filter((path) => !hrefs.includes(path));
 	const favicons = await Promise.all([
-		...hrefs.map((href) => getFaviconFrom(processURL(isRelativeURL(href) ? url + href : href, false))),
+		...hrefs
+			.filter((href) => (isRelativeURL(href) && !url ? false : true))
+			.map((href) => getFaviconFrom(processURL(isRelativeURL(href) ? url + href : href, false))),
 		...filteredPaths.map(getFaviconFrom),
 	]);
 	return favicons.filter((x): x is Favicon => x !== undefined);
--- packages/npm/src/index.ts
+++ packages/npm/src/index.ts
@@ -2,7 +2,7 @@ import * as cheerio from "cheerio";
 import { fetchWithTimeout } from "./util";
 import { processURL, isRelativeURL } from "./util";
 
-import { requestFavicons } from "./fetcher";
+import { getFaviconsFrom } from "./fetcher";
 
 import type { Favicon } from "./types";
 
@@ -12,6 +12,20 @@ interface Options {
 	timeout?: number;
 }
 
+/**
+ * Extract all favicons from the given URL
+ *
+ * @example
+ * ```ts
+ * import { getFavicons } from "favicons";
+ *
+ * const favicons = await getFavicons("nextjs.com");
+ * ```
+ *
+ * @param url
+ * @param options
+ * @returns Favicon[]
+ */
 export async function getFavicons(
 	url: string,
 	options: Options = {
@@ -19,30 +33,55 @@ export async function getFavicons(
 	}
 ): Promise<Favicon[]> {
 	return new Promise(async (resolve, reject) => {
-		// ensure there is a valid url
 		url = processURL(url);
 
 		if (isRelativeURL(url)) throw new Error("Only absolute urls are valid.");
 
 		try {
 			const result = await withRequest(url, options.timeout);
-			result.length ? resolve(result) : reject(new Error("No favicons found."));
+			if (result.length) resolve(result);
+			else throw new Error("No favicons found for the given url.");
 		} catch (error) {
 			reject(error);
 		}
 	});
 }
 
+/**
+ * Get the best favicon from the given URL
+ *
+ * @example
+ * ```ts
+ * import { getFavicon } from "favicons";
+ *
+ * const favicon = await getFavicon("nextjs.com");
+ * ```
+ *
+ * @param url
+ * @param options
+ * @returns Favicon
+ */
 export async function getFavicon(url: string, options: Options): Promise<Favicon> {
 	const favicons = await getFavicons(url, options);
 	return favicons.sort((a, b) => b.size - a.size)[0];
 }
 
-async function withRequest(url: string, timeout: number = 5000) {
-	const response = await fetchWithTimeout(url, timeout);
-	if (!response || !response.ok) throw new Error(`Failed to fetch ${url}`);
-	const html = await response.text();
-
+/**
+ * Get the favicon urls from the given HTML (or head element)
+ *
+ * @example
+ * ```ts
+ * import { extractFavicons } from "favicons";
+ *
+ * const request = await fetch("https://nextjs.com");
+ * const html = await request.text();
+ * const favicons = extractFavicons(html);
+ * ```
+ *
+ * @param html - html string of a website or head element specifically (faster)
+ * @returns string[] - array of favicon urls
+ */
+export async function extractFavicons(html: string) {
 	const $ = cheerio.load(html);
 	const hrefs = rels
 		.map((rel) => {
@@ -52,7 +91,17 @@ async function withRequest(url: string, timeout: number = 5000) {
 		})
 		.flat();
 
-	return requestFavicons(url, hrefs);
+	return hrefs;
+}
+
+async function withRequest(url: string, timeout: number = 5000) {
+	const response = await fetchWithTimeout(url, timeout);
+	if (!response || !response.ok) throw new Error(`Failed to fetch ${url}`);
+	const html = await response.text();
+
+	const hrefs = await extractFavicons(html);
+
+	return getFaviconsFrom(hrefs, url);
 }
 
 export type { Favicon } from "./types";
