d701081e8c911f1a1565fe11565b73fd18f0fc67
Andrés Martínez <andresmarpz@gmail.com>
2023-02-11 19:02:31 -0300

    compress output and add fetch headers

--- packages/npm/src/util.ts
+++ packages/npm/src/util.ts
@@ -1,4 +1,4 @@
-import fetch, { Response } from 'node-fetch';
+import fetch, { Response } from "node-fetch";
 
 export function processURL(url: string, removeParams: boolean = true): string {
 	if (isRelativeURL(url)) return url;
@@ -26,6 +26,19 @@ export function fetchWithTimeout(url: string, ms: number = 5000) {
 
 		fetch(url, {
 			signal,
+			headers: {
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
+				Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+				"Accept-Language": "en-US,en;q=0.5",
+				"Accept-Encoding": "gzip, deflate",
+				Connection: "keep-alive",
+				"Upgrade-Insecure-Requests": "1",
+				"Sec-Fetch-Dest": "document",
+				"Sec-Fetch-Mode": "navigate",
+				"Sec-Fetch-Site": "none",
+				"Sec-Fetch-User": "?1",
+				"Cache-Control": "max-age=0",
+			},
 		})
 			.then((response: Response) => resolve(response))
 			.catch((error) => reject(error))
